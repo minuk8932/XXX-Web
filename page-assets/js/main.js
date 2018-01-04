077db7c3f63b5c7e1649ef6446f6e9e47abcacf9
minuk8932 <minuk8932@naver.com>
2018-01-04 18:15:17 +0900

    add menu popup

--- page-assets/js/main.js
+++ page-assets/js/main.js
@@ -1,7 +1,13 @@
+$( document ).ready(function() {
+    // 팝업 열기 트리거
+    $('.triggerMenu').click(function(){ 
+        $('#popup_layer, #overlay_t').show(); 
+        $('#popup_layer').css("top", Math.max(0, $(window).scrollTop() + 100) + "px");  
+    });
 
-
-
-/* $ jQuery 사용 선언, 내부에서만 jQuery를 정의 */
-(function($) {
-    
-})(jQuery);
+    // 닫기 이벤트
+    $('#overlay_t, .close').click(function(e){ 
+         e.preventDefault(); 
+         $('#popup_layer, #overlay_t').hide(); 
+    }); 
+});
