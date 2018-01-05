873e1465f05d3f2cb29b7c9fba369bead42d56b3
minuk8932 <minuk8932@naver.com>
2018-01-05 17:18:59 +0900

    setting main popup

--- page-assets/js/main.js
+++ page-assets/js/main.js
@@ -1,13 +1,24 @@
 $( document ).ready(function() {
     // 팝업 열기 트리거
+
     $('.triggerMenu').click(function(){ 
-        $('#popup_layer, #overlay_t').show(); 
+        $('#popup_layer, #overlay_t').show();
+        $('.triggerMenu').hide();
         $('#popup_layer').css("top", Math.max(0, $(window).scrollTop() + 100) + "px");  
     });
 
     // 닫기 이벤트
-    $('#overlay_t, .close').click(function(e){ 
-         e.preventDefault(); 
-         $('#popup_layer, #overlay_t').hide(); 
-    }); 
+    // $('#overlay_t, .close').click(function(e){ 
+    //     e.preventDefault(); 
+    //     $('#popup_layer, #overlay_t').hide();
+    // });
+
+    $(document).ready(function() {
+        $('.triggerPop').click(function() {
+            $('#overlay_t').hide();
+            $('#popup_layer').hide();
+            $('.triggerMenu').show();
+        });
+    });
+    
 });
