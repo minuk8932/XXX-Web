26a5369b5fb7bee269f55fd0feb259a8a909ed44
minuk8932 <minuk8932@naver.com>
2018-01-15 17:10:37 +0900

    add dropbtn img event

--- page-assets/js/main.js
+++ page-assets/js/main.js
@@ -19,3 +19,17 @@ $( document ).ready(function() {
         });
     });
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scrollT = $(this).scrollTop();  // 스크롤 바 상단 위치 값
+        var menuH = $('#shop-items').offset(); // 스크롤 바가 존재하는 div의 높이 값
+        
+        if(scrollT > menuH.top){                     // 스크롤 한 경우
+            $('.first-dropbtn').css('z-index', 1);   // 이미지를 다른 dropbtn과 같은 설정
+        }
+        else{                                        // 스크롤 안 한 경우
+            $('.first-dropbtn').css('z-index', 200); // 제일 바깥으로 끌어내 이미지가 보일 수 있게 설정
+        }
+    });
+});
